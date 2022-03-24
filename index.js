ec816961a10250d6f6eca02d8f55345153007395
unknown <sistemas.cesavesin@gmail.com>
2022-03-24 11:46:02 -0700

    actualizacion

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
 var app = (0, express_1.default)();
-var port = 3030;
+var port = 3000;
 app.get('/', function (req, res) {
     res.json({
         ok: true,
