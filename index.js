3c4de941661de2d87c6a9216c91b1fd6679c508c
unknown <sistemas.cesavesin@gmail.com>
2022-03-24 11:25:39 -0700

    incial

---
+++ index.js
@@ -0,0 +1,17 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var app = (0, express_1.default)();
+var port = 3030;
+app.get('/', function (req, res) {
+    res.json({
+        ok: true,
+        message: 'Todo bien...'
+    });
+});
+app.listen(port, function () {
+    console.log("Servidor en el puerto:" + port);
+});
